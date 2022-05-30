db4a05f66a19957693983f2e9a7c6bb873a8f015
mickeyymousee <sarah.mansoor@mail.utoronto.ca>
2022-05-30 11:33:49 -0400

    Update 01-data_cleaning.R

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,1 +1,79 @@
+#### Preamble ####
+# Purpose: Clean the data downloaded from Toronto Open Data Portal
+# Author: Sarah Mansoor
+# Data: May 2022
+# Contact: sarah.mansoor@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the ACS data and saved it to inputs/data
+# - Don't forget to gitignore it!
+
+
+#### Workspace setup ####
+# Use R Projects, not setwd().
+library(tidyverse)
+library(opendatatoronto)
+library(dplyr)
+
+# Cleaning 2015 data
+View(data_2015)
+
+colnames(data_2015) <- c("Operation Name", "Operation Type", "Address",
+                         "City", "Postal Code", "Total Floor Area",
+                         "Unit", "Avg hrs/wk", "Annual Flow (Mega Liters)",
+                         "Electricity Quantity", "Electricity Unit",
+                         "Natural Gas Quantity", "Natural Gas Unit", 
+                         "Fuel Oil 1 & 2 Quantity", "Fuel Oil 1 & 2 Unit", 
+                         "Fuel Oil 4 & 6 Quantity", "Fuel Oil 4 & 6 Unit",
+                         "Propane Quantity", "Propoane Unit", "Coal Quantity",
+                         "Coal Unit", "Wood Quantity", "Wood Unit", "District Heating Quantity",
+                         "District Heating Unit", "District Hearing Renewable", "District Heating Emission Factor",
+                         "District Cooling Quantity", "District Cooling Unit", "District Cooling Renewable",
+                         "District Cooling Emission Factor", "GHG Emissions (kg)", "Energy Intensity (ekWh/sqft)",
+                         "Energy Intensity (ekWh/Mega Litre", "Building/Operation Identifier", "Comments")
+
+# Cleaning 2016 data
+colnames(data_2016) <- c("Operation Name", "Operation Type", "Address",
+                         "City", "Postal Code", "Total Floor Area",
+                         "Unit", "Avg hrs/wk", "Annual Flow (Mega Liters)",
+                         "Electricity Quantity", "Electricity Unit",
+                         "Natural Gas Quantity", "Natural Gas Unit", 
+                         "Fuel Oil 1 & 2 Quantity", "Fuel Oil 1 & 2 Unit", 
+                         "Fuel Oil 4 & 6 Quantity", "Fuel Oil 4 & 6 Unit",
+                         "Propane Quantity", "Propoane Unit", "Coal Quantity",
+                         "Coal Unit", "Wood Quantity", "Wood Unit", "District Heating Quantity",
+                         "District Heating Unit", "District Hearing Renewable", "District Heating Emission Factor",
+                         "District Cooling Quantity", "District Cooling Unit", "District Cooling Renewable",
+                         "District Cooling Emission Factor", "GHG Emissions (kg)", "Energy Intensity (ekWh/sqft)",
+                         "Energy Intensity (ekWh/Mega Litre", "Building/Operation Identifier", "Comments")
+
+# Cleaning 2017 data
+colnames(data_2017) <- c("Operation Name", "Operation Type", "Address",
+                         "City", "Postal Code", "Total Floor Area",
+                         "Unit", "Avg hrs/wk", "Annual Flow (Mega Liters)",
+                         "Electricity Quantity", "Electricity Unit",
+                         "Natural Gas Quantity", "Natural Gas Unit", 
+                         "Fuel Oil 1 & 2 Quantity", "Fuel Oil 1 & 2 Unit", 
+                         "Fuel Oil 4 & 6 Quantity", "Fuel Oil 4 & 6 Unit",
+                         "Propane Quantity", "Propoane Unit", "Coal Quantity",
+                         "Coal Unit", "Wood Quantity", "Wood Unit", "District Heating Quantity",
+                         "District Heating Unit", "District Hearing Renewable", "District Heating Emission Factor",
+                         "District Cooling Quantity", "District Cooling Unit", "District Cooling Renewable",
+                         "District Cooling Emission Factor", "GHG Emissions (kg)", "Energy Intensity (ekWh/sqft)",
+                         "Energy Intensity (ekWh/Mega Litre", "Building/Operation Identifier", "Comments")
+
+# Cleaning 2018 data
+colnames(data_2018) <- c("Operation Name", "Operation Type", "Address",
+                         "City", "Postal Code", "Total Floor Area",
+                         "Unit", "Avg hrs/wk", "Annual Flow (Mega Liters)",
+                         "Electricity Quantity", "Electricity Unit",
+                         "Natural Gas Quantity", "Natural Gas Unit", 
+                         "Fuel Oil 1 & 2 Quantity", "Fuel Oil 1 & 2 Unit", 
+                         "Fuel Oil 4 & 6 Quantity", "Fuel Oil 4 & 6 Unit",
+                         "Propane Quantity", "Propoane Unit", "Coal Quantity",
+                         "Coal Unit", "Wood Quantity", "Wood Unit", "District Heating Quantity",
+                         "District Heating Unit", "District Hearing Renewable", "District Heating Emission Factor",
+                         "District Cooling Quantity", "District Cooling Unit", "District Cooling Renewable",
+                         "District Cooling Emission Factor", "GHG Emissions (kg)", "Energy Intensity (ekWh/sqft)",
+                         "Energy Intensity (ekWh/Mega Litre", "Building/Operation Identifier", "Comments")
 
