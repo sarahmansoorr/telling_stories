82bde851d41076c62969915b8af5bab5b1b7502b
mickeyymousee <sarah.mansoor@mail.utoronto.ca>
2022-05-31 13:22:45 -0400

    Update 01-data_cleaning.R

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -14,6 +14,8 @@
 library(tidyverse)
 library(opendatatoronto)
 library(dplyr)
+library(ggplot2)
+library(janitor)
 
 # Load Data
 
@@ -37,17 +39,83 @@ my_column_names <- c("Operation Name", "Operation Type", "Address",
                      "District Cooling Emission Factor", "GHG Emissions (kg)", "Energy Intensity (ekWh/sqft)",
                      "Energy Intensity (ekWh/Mega Litre", "Building/Operation Identifier", "Comments")
 
-# Cleaning 2015 data
+### Cleaning 2015 data ###
 colnames(data_2015) <- my_column_names
 
+# Remove first 7 rows
+# ...
+
+# Clean using janitor and dplyr
+data_2015 <- clean_names(data_2015)
+data_2015 <- remove_empty(data_20151, which = c("rows", "cols"),
+                           quiet = FALSE)
+data_2015 <- distinct(data_20152)
+
+data_2015 <- data_2015 %>% select(operation_name, operation_type, 
+                                   city, total_floor_area, avg_hrs_wk, 
+                                   annual_flow_mega_liters, electricity_quantity, 
+                                   natural_gas_quantity, ghg_emissions_kg, 
+                                   energy_intensity_ek_wh_sqft,
+                                   energy_intensity_ek_wh_mega_litre)
+data_20151 <- data_2015 %>% drop_na()
 
 # Cleaning 2016 data
 colnames(data_2016) <- my_column_names
 
+# Remove first 7 rows
+# ...
+
+# Clean using janitor and dplyr
+data_2016 <- clean_names(data_2016)
+data_2016 <- remove_empty(data_2016, which = c("rows", "cols"),
+                          quiet = FALSE)
+data_2016 <- distinct(data_2016)
+
+data_2016 <- data_2016 %>% select(operation_name, operation_type, 
+                                  city, total_floor_area, avg_hrs_wk, 
+                                  annual_flow_mega_liters, electricity_quantity, 
+                                  natural_gas_quantity, ghg_emissions_kg, 
+                                  energy_intensity_ek_wh_sqft,
+                                  energy_intensity_ek_wh_mega_litre)
+data_20161 <- data_2016 %>% drop_na()
+
 # Cleaning 2017 data
 colnames(data_2017) <- my_column_names
+
+# Remove first 7 rows
+# ...
+
+# Clean using janitor and dplyr
+data_2017 <- clean_names(data_2017)
+data_2017 <- remove_empty(data_2017, which = c("rows", "cols"),
+                          quiet = FALSE)
+data_2017 <- distinct(data_2017)
+
+data_2017 <- data_2017 %>% select(operation_name, operation_type, 
+                                  city, total_floor_area, avg_hrs_wk, 
+                                  annual_flow_mega_liters, electricity_quantity, 
+                                  natural_gas_quantity, ghg_emissions_kg, 
+                                  energy_intensity_ek_wh_sqft,
+                                  energy_intensity_ek_wh_mega_litre)
+data_20171 <- data_2017 %>% drop_na()
 
 # Cleaning 2018 data
 colnames(data_2018) <- my_column_names
 
+# Remove first 7 rows
+# ...
+
+# Clean using janitor and dplyr
+data_2018 <- clean_names(data_2018)
+data_2018 <- remove_empty(data_2018, which = c("rows", "cols"),
+                          quiet = FALSE)
+data_2018 <- distinct(data_2018)
+
+data_2018 <- data_2018 %>% select(operation_name, operation_type, 
+                                  city, total_floor_area, avg_hrs_wk, 
+                                  annual_flow_mega_liters, electricity_quantity, 
+                                  natural_gas_quantity, ghg_emissions_kg, 
+                                  energy_intensity_ek_wh_sqft,
+                                  energy_intensity_ek_wh_mega_litre)
+data_20181 <- data_2018 %>% drop_na()
 
